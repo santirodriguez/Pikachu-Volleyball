2465e8525e927f2520a3ae3032804d57f73e1ee5
santirodriguez <15202529+santirodriguez@users.noreply.github.com>
2026-08-05 13:19:07 -0300

    Copy the menu prototype stylesheet

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,10 @@ module.exports = {
         { from: 'src/ko/manifest.json', to: 'ko/manifest.json' },
         { from: 'src/zh/manifest.json', to: 'zh/manifest.json' },
         { from: 'src/resources/style.css', to: 'resources/style.css' },
+        {
+          from: 'src/resources/menu-prototype.css',
+          to: 'resources/menu-prototype.css',
+        },
         { from: 'src/index.html', to: 'index.html' },
       ],
     }),
